8c7c542c7fa78f05a939f34ebdc5c1271354fe03
Joshua Stolle <joshuastolle445@gmail.com>
2024-01-11 15:10:10 +0100

    feat: my first project schema to Sanity configuration
    A new schema has been added to the Sanity configuration named 'project', which includes fields for name, slug, image, url, and content. Alongside, the main 'schema.ts' file has been updated to include this new schema. Code formatting has been improved according to Prettier rules for better readability.

---
+++ sanity/project-schema.ts
@@ -0,0 +1,52 @@
+const project = {
+  name: 'project',
+  title: 'Projects',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Project Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'alt',
+          title: 'Alternative Text',
+          type: 'string',
+          options: {
+            isHighlighted: true,
+          },
+        },
+      ],
+    },
+    {
+      name: 'url',
+      title: 'URL',
+      type: 'url',
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [{ type: 'block' }],
+    },
+  ],
+};
+
+export default project;
