d9038af9d9de4eeb9ad111226ac7ed37873c37e7
Joshua Stolle <joshuastolle445@gmail.com>
2024-01-12 10:27:24 +0100

    feat: main page title and tailwindcss
    The main page now displays a title, "Sanity Next14 App", enhancing page identification. Accompanying this, layout adjustments were made by incorporating extra CSS classes. Consequently, these updates aim to boost user engagement through improved page design and comprehension.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,10 @@ export default async function Home() {
   const projects = await getProjects();
 
   return (
-    <main className="flex-col justify-center mt-8">
+    <main className="flex flex-col justify-center items-center mt-8">
+      <h1 className="font-extrabold text-4xl my-8 inline-block bg-gradient-to-r from-green-400 to-blue-800 bg-clip-text text-transparent">
+        Sanity Next14 App
+      </h1>
       {projects.map((project) => (
         <div key={project._id} className="flex flex-col items-center">
           <h1 className="text-2xl font-bold">{project.name}</h1>
