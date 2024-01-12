c47b30512978034b844cdfead7089ba2d51eb499
Joshua Stolle <joshuastolle445@gmail.com>
2024-01-12 17:18:06 +0100

    feat: revalidation api

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -47,6 +47,7 @@ export async function POST(req: NextRequest) {
     if (body.slug) {
       revalidateTag(`${body._type}:${body.slug}`);
     }
+
     return NextResponse.json({
       status: 200,
       revalidated: true,
