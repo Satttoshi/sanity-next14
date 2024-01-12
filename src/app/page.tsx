640c8af1da24c763565e229159ac95a363d4917d
Joshua Stolle <joshuastolle445@gmail.com>
2024-01-12 17:18:17 +0100

    refactor: sanity utility and update page.tsx
    The 'sanity-utils.ts' file was deleted and replaced with a new 'loadQuery' loader that is now utilized in 'page.tsx'. This modification ensures a more thorough fetch validation process for projects. Also, error handling has been incorporated to display a message when no entries are found in the CMS.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
-import { getProjects } from '../../sanity/sanity-utils';
 import Image from 'next/image';
+import { loadProjects } from '../../sanity/loaders/loadQuery';
 
 export default async function Home() {
   // since this is a static page, we can fetch the data at build time, no need for client side fetching and custom getStaticProps
-  const projects = await getProjects();
+  const projects = await loadProjects();
+  if (!projects.data) {
+    return <div>No entries in CMS</div>;
+  }
 
   return (
     <main className="flex flex-col justify-center items-center mt-8">
       <h1 className="font-extrabold text-4xl my-8 inline-block bg-gradient-to-r from-green-400 to-blue-800 bg-clip-text text-transparent">
         Sanity Next14 App
       </h1>
-      {projects.map((project) => (
+      {projects.data.map((project) => (
         <div key={project._id} className="flex flex-col items-center">
           <h1 className="text-2xl font-bold">{project.name}</h1>
           {project.image && (
