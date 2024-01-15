158f289d96ba8c5ba6d2010102d4a17ddbb0d9c9
Joshua Stolle <joshuastolle445@gmail.com>
2024-01-15 16:13:44 +0100

    test: set of testlogs for api call

--- logger.js
+++ logger.js
@@ -7,7 +7,7 @@ const logger = winston.createLogger({
   format: combine(timestamp(), json()),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'tmp/combined.log' }),
+    new winston.transports.File({ filename: '/tmp/combined.log' }),
   ],
 });
 
